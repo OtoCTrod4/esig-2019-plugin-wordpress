75ff5e41181eedeb7cc3a7df7e19846e4653f969
Melissa Plomb <melissa.plomb@divcom.ch>
2019-05-20 10:55:10 +0200

    Corrections du code sur les actions

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -3,8 +3,15 @@
 Plugin Name: Mon Premier Plugin
 */
 
+//Fonction qui affiche la balise meta
+function mon_premier_plugin_meta_keywords() {
+    echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
+}
+//Ajout d'une action sur 'wp_head' qui appellera mon_premier_plugin_meta_keywords()
+add_action('wp_head', 'mon_premier_plugin_meta_keywords' );
+
 //Fonction qui envoie par email les infos d'un email supprimé
-function mon_plugin_post_delete_mail($post_id) {
+function mon_premier_plugin_post_delete_mail($post_id) {
     //Récupére les informations de l'article supprimé
     $post = get_post($post_id);
     //Création du sujet de l'email
@@ -14,5 +21,5 @@ function mon_plugin_post_delete_mail($post_id) {
     //Envoi de l'email à l'administrateur du site
     wp_mail(get_bloginfo('admin_email'), $sujet, $message);
 }
-//Ajout d'une action sur 'delete_post' qui appellera mon_plugin_post_delete_mail()
-add_action('delete_post', 'mon_plugin_post_delete_mail');
+//Ajout d'une action sur 'delete_post' qui appellera mon_premier_plugin_post_delete_mail()
+add_action('delete_post', 'mon_premier_plugin_post_delete_mail');
