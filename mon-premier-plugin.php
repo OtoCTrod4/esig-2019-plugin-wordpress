f293e12a38d91caa7b4d0e217cceff27f9390beb
Melissa Plomb <melissa.plomb@divcom.ch>
2019-05-20 09:59:05 +0200

    Action sur le crocher wp-head

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -2,3 +2,10 @@
 /**
 Plugin Name: Mon premier plugin
  */
+
+//Fonction qui affiche la balise meta
+function mon_premier_plugin_meta_keywords() {
+    echo '<meta name="keywords" content="HTML,CSS,XML,JavaScript">';
+}
+//Ajout d'une action sur 'wp_head' qui appellera mon_plugin_meta_keywords()
+add_action('wp_head', 'mon_plugin_meta_keywords' );
